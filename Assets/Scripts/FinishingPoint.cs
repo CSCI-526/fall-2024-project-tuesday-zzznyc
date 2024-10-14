a613cf76737fcaefb3e3f45a8df657a479132865
Zhiheng Wang <69889580+zwang203@users.noreply.github.com>
2024-10-13 23:01:01 -0700

    win trigger

--- Assets/Scripts/FinishingPoint.cs
+++ Assets/Scripts/FinishingPoint.cs
@@ -5,24 +5,9 @@ using TMPro;
 
 public class FinishingPoint : MonoBehaviour
 {   
-    [SerializeField] TextMeshProUGUI winText;
-    // Start is called before the first frame update
-    void Start()
-    {
-        winText.gameObject.SetActive(false);
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player") && winText != null)
         {
-            winText.gameObject.SetActive(true);
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
