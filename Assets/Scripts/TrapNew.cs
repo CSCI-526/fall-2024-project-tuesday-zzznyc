ec53fa66b77ce6684ac909dc3e4d4e3261ede12a
ChenhaoYi-n <cyin1970@usc.edu>
2024-11-29 17:19:21 -0800

    Level4 update

--- Assets/Scripts/TrapNew.cs
+++ Assets/Scripts/TrapNew.cs
@@ -4,27 +4,23 @@ using UnityEngine;
 
 public class TrapLV3 : MonoBehaviour
 {
-    // 设置玩家传送位置
     public Vector2 respawnPosition = new Vector2(0, 0);
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            // 将玩家传送到固定位置
             collision.gameObject.transform.position = respawnPosition;
-
-            // 停止玩家一切运动
             Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
             if (rb != null)
             {
-                rb.velocity = Vector2.zero;  // 停止线性速度
-                rb.angularVelocity = 0f;    // 停止角速度
+                rb.velocity = Vector2.zero;  
+                rb.angularVelocity = 0f;    
             }
         }
         else if (collision.gameObject.CompareTag("BOOM"))
         {
-            // 子弹碰到陷阱时摧毁子弹
+            Destroy(collision.gameObject);
             Destroy(gameObject);
         }
     }
